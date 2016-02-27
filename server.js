7f7af985fc1bc8c5ed23333ffb532f6a70a0d7aa
Jon Ah-Nee <jonahnee@gmail.com>
2016-02-27 09:58:18 -1000

    angular loaded on page

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
+
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 var server = app.listen(3000, function() {
   console.log('Server listening on port', server.address().port);
