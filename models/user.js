65dff78cb2ec2842526ce7b99dde28416ff05130
Jon Ah-Nee <jonahnee@gmail.com>
2016-04-25 21:54:31 -1000

    updating

--- models/user.js
+++ models/user.js
@@ -1,3 +1,29 @@
+// 'use strict';
+// module.exports = function(sequelize, DataTypes) {
+//   var user = sequelize.define('user', {
+//     username: {
+//       type: DataTypes.STRING,
+//       allowNull: false
+//       // set: function(val) {
+//       //   this.setDataValue('username', val.toLowerCase());
+//       // }
+//     },
+//     password: {
+//       type: DataTypes.STRING,
+//       allowNull: false
+//       // set: function(val) {
+//       //   this.setDataValue('password', val.toLowerCase());
+//       // }
+//     },
+//     classMethods: {
+//       associate: function(models) {
+//         // associations can be defined here
+//       }
+//     }
+//   });
+//   return user;
+// };
+
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var user = sequelize.define('user', {
